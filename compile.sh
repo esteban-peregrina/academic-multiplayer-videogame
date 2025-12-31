519155907c67f2e6bd6210bce6130d76b89f31f2
Esteban Peregrina <110783029+esteban-peregrina@users.noreply.github.com>
2025-12-31 18:44:33 +0100

    FIX: Code can now be executed and players can connect

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-gcc -o ./bin/sh13 -I/usr/include/SDL2 ./src/sh13.c -lSDL2_image -lSDL2_ttf -lSDL2 -lpthread
-gcc -o ./bin/server ./src/server.c
+gcc -o ./bin/Sherlock13 -I/usr/include/SDL2 ./src/client.c -lSDL2_image -lSDL2_ttf -lSDL2 -lpthread
+gcc -o ./bin/Serveur ./src/server.c
